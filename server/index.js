b8b04cf2de088c0377ac48c2e9640cdfe35da42f
kumarankit0008 <131030340+kumarankit0008@users.noreply.github.com>
2023-04-25 12:37:49 +0500

    work complete

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.static("uploads")); //image upload
 app.use(cookieParser());
-app.use(cors());
+app.use(cors();
 // database connection is here
 const DB_URI = process.env.MONGO_URI;
 
