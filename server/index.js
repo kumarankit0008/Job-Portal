a15ce09a0d9a144f84d2552852d8012d0f603c31
kumarankit0008 <131030340+kumarankit0008@users.noreply.github.com>
2023-04-25 12:04:33 +0500

    work complete

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.static("uploads")); //image upload
 app.use(cookieParser());
-app.use(cors({ origin: "https://job-portal-flame-one.vercel.app", credentials: true }));
+app.use(cors({ origin: "https://job-portal-flame-one.vercel.app/", credentials: true }));
 // database connection is here
 const DB_URI = process.env.MONGO_URI;
 
