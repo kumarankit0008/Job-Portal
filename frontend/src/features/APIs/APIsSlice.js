23ed64adee7b4c703b6910d746795b7c4ae358a7
kumarankit0008 <131030340+kumarankit0008@users.noreply.github.com>
2023-04-25 13:21:00 +0500

    work complete

--- frontend/src/features/APIs/APIsSlice.js
+++ frontend/src/features/APIs/APIsSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const APIsSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://job-portal-backend-six.vercel.app/",
+    baseUrl: "https://job-portal-backend-flax.vercel.app/",
     prepareHeaders: async (headers, { getState, endpoint }) => {
       const token = getState()?.auth?.accessToken;
       // console.log(token);
